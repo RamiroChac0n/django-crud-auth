74caae5732137ee6c28968376e822c5c49b410d9
Ramiro <ramiro20010202@gmail.com>
2025-12-04 20:14:46 -0600

    registro de usuarios

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
 def signup(request):
-    return render(request, 'signup.html',{
-        'form':UserCreationForm()
-    })
+    if request.method == 'GET':
+        return render(request, 'signup.html',{
+            'form':UserCreationForm()
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'],
+                    password=request.POST['password1'],
+                )
+                user.save()
+                return redirect('home')
+            except:
+                return HttpResponse('User already exists')            
+        else:
+            return HttpResponse('Your passwords do not match')
