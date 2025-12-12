4cb786d91727091d144e5928564437166ab3a530
Ramiro <ramiro20010202@gmail.com>
2025-12-11 23:08:18 -0600

    manejando errores

--- tasks/views.py
+++ tasks/views.py
@@ -4,13 +4,16 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
 
+
 def signup(request):
     if request.method == 'GET':
-        return render(request, 'signup.html',{
-            'form':UserCreationForm()
+        return render(request, 'signup.html', {
+            'form': UserCreationForm()
         })
     else:
         if request.POST['password1'] == request.POST['password2']:
@@ -22,6 +25,12 @@ def signup(request):
                 user.save()
                 return redirect('home')
             except:
-                return HttpResponse('User already exists')            
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm(),
+                    'error': 'User already exists'
+                })
         else:
-            return HttpResponse('Your passwords do not match')
+            return render(request, 'signup.html', {
+                'form': UserCreationForm(),
+                'error': 'Passwords do not match'
+            })
