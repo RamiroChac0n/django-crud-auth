4ff24733c095ace61772639572cdbfc04a435793
Ramiro <ramiro20010202@gmail.com>
2025-12-13 21:50:50 -0600

    logout

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.db import IntegrityError
 
 # Create your views here.
@@ -37,6 +37,10 @@ def signup(request):
                 'form': UserCreationForm(),
                 'error': 'Passwords do not match'
             })
+        
+def signout(request):
+    logout(request)
+    return redirect('home')
 
 def tasks(request):
     return render(request, 'tasks.html')
